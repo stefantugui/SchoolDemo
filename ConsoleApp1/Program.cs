f136d6a4d5325d5ba596fdaeea2f71502f7444c7
stefantugui <stefantugui@users.noreply.github.com>
2021-03-08 08:46:25 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace SchoolDemo
+{
+    using System;
+    using System.Threading;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action a;
+
+            var testObject = new TestClass();
+
+            testObject.doWork();
+            Thread.Sleep(1);
+        }
+    }
+}
---
+++ ConsoleApp1/TestClass.cs
@@ -0,0 +1,26 @@
+﻿namespace SchoolDemo
+{
+    using System;
+
+    public class TestClass
+    {
+        public int numberOfRepetitions = 2;
+
+        public string StringProp { get; set; }
+
+        public void doWork()
+        {
+            char[] letters = { 'f', 'r', 'e', 'd', ' ', 's', 'm', 'i', 't', 'h' };
+            string name = "";
+            int[] a = new int[10];
+            for (var j = 0; j < numberOfRepetitions; j++)
+                for (int i = 0; i < letters.Length; i++)
+                {
+                    name += letters[i];
+                    a[i] = i + 1;
+                    Console.WriteLine("Hello, " + name + "! Count to " + a[i]);
+                }
+            Console.ReadKey();
+        }
+    }
+}
